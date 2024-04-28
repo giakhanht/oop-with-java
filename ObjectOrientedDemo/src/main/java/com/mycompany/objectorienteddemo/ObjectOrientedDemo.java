c891bdb2419787ecff03a553895a14b739ad84ab
giakhanht <giakhanht.cs@gmail.com>
2024-04-28 12:16:27 +0200

    Create objects and implement methods using Staff class

---
+++ ObjectOrientedDemo/src/main/java/com/mycompany/objectorienteddemo/ObjectOrientedDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.objectorienteddemo;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ObjectOrientedDemo {
+    public static void main(String[] args) {
+        Staff staff1 = new Staff("Peter");
+        staff1.setHoursWorked(160);
+        int pay = staff1.calculatePay(1000, 400);
+        System.out.println("Pay = " + pay);
+        
+        Staff staff2 = new Staff("James", "Lee");
+        staff2.setHoursWorked(300);
+        pay = staff2.calculatePay();
+        System.out.println("Pay = " + pay);
+    }
+}
